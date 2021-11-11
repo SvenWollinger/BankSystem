45641442627386dfd7317e4b0f7e06dc5298eca8
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-11-11 12:33:35 +0100

    change DEBUG to false

--- src/main/java/io/wollinger/banksystem/Main.java
+++ src/main/java/io/wollinger/banksystem/Main.java
@@ -1,7 +1,7 @@
 package io.wollinger.banksystem;
 
 public class Main {
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = false;
     public static void main(String[] args) {
         new BankSystem().showMenu(BankSystem.MenuPage.MAIN);
     }
